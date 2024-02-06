ac17b82406934df54278be81257930b1595478f1
Kian McKenna <kian@mckenna.cloud>
2024-02-06 16:14:24 -0600

    Add @vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,31 @@
 <!-- src/routes/+layout.svelte -->
 <script>
-    import "../app.pcss";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
 
-    import { invalidate } from '$app/navigation'
-    import { onMount } from 'svelte'
-  
-    export let data
-  
-    let { supabase, session } = data
-    $: ({ supabase, session } = data)
-  
-    onMount(() => {
-      const {
-        data: { subscription },
-      } = supabase.auth.onAuthStateChange((event, _session) => {
-        if (_session?.expires_at !== session?.expires_at) {
-          invalidate('supabase:auth')
-        }
-      })
-  
-      return () => subscription.unsubscribe()
+  import { invalidate } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  import "../app.pcss";
+
+  inject({ mode: dev ? "development" : "production" });
+
+  export let data;
+
+  let { supabase, session } = data;
+  $: ({ supabase, session } = data);
+
+  onMount(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, _session) => {
+      if (_session?.expires_at !== session?.expires_at) {
+        invalidate("supabase:auth");
+      }
     });
-  </script>
-  
-  <slot />
+
+    return () => subscription.unsubscribe();
+  });
+</script>
+
+<slot />
