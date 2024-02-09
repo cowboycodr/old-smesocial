edfd5a2d7997196f69df8123254f9093aead7a60
Kian McKenna <kian@mckenna.cloud>
2024-02-08 19:25:22 -0600

    modify sign up flow

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,7 @@
 // src/routes/+layout.js
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
+import { redirect } from '@sveltejs/kit'
 
 export const load = async ({ fetch, data, depends }) => {
   depends('supabase:auth')
@@ -16,5 +17,19 @@ export const load = async ({ fetch, data, depends }) => {
     data: { session },
   } = await supabase.auth.getSession()
 
-  return { supabase, session }
+  if (!session) {
+    return { supabase, session, profile: null }
+  }
+
+  const { data: profile } = await supabase
+    .from('profiles')
+    .select('*')
+    .eq('id', session?.user?.id)
+    .single()
+
+  if (!profile?.setup) {
+    return redirect(303, '/profile/setup')
+  }
+
+  return { supabase, session, profile }
 }
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,22 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ fetch, locals: { supabase, getSession } }) => {
-    const session = await getSession();
-
-    let profile = undefined;
-
-    if (session) {
-        ({ data: profile } = await supabase
-            .from('profiles')
-            .select('*')
-            .eq('id', session.user.id)
-            .single());
-
-        if (!profile?.setup) {
-            return redirect(303, "/profile/setup")
-        }
-    }
-
     const getFeed = async () => {
         const response = await fetch("/api/v1/posts");
         const posts = await response.json();
@@ -26,5 +10,5 @@ export const load = async ({ fetch, locals: { supabase, getSession } }) => {
 
     const feed = await getFeed();
 
-    return { profile, feed };
+    return { feed };
 }
